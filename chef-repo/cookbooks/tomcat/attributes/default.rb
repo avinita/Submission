dd460967b123ae7efcedf4ed535a63ad2f4ea214
avinita <avinita.mohanty@gmail.com>
2017-03-05 21:16:52 -0500

    Update default.rb

--- chef-repo/cookbooks/tomcat/attributes/default.rb
+++ chef-repo/cookbooks/tomcat/attributes/default.rb
@@ -1,3 +1,7 @@
+# AVINITA MOHANTY
+# CARNEGIE MELLON UNIVERSITY
+# 5 MARCH 2017
+
 default["directory_tomcat"] = "/opt/apache-tomcat-8.5.11.tar.gz"
 default["cleanup_jdk"] = "/opt/jdk-8u121-linux-x64.tar.gz"
 default["cleanup_jre"] = "/opt/jre-8u121-linux-x64.tar.gz"
